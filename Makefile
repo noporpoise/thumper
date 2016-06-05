29894198d98420575d95a327724259451f2ebca3
Isaac Turner <turner.isaac@gmail.com>
2016-06-05 19:21:27 +0100

    Added block option

--- Makefile
+++ Makefile
@@ -3,13 +3,13 @@
 all: thumper
 
 thumper: thumper.c
-	$(CC) -Wall -Wextra -o $@ $<
+	$(CC) -Wall -Wextra -O2 -o $@ $<
 
 clean:
 	rm -rf thumper test.thumper.txt
 
 test: thumper
-	./thumper test.thumper.txt
+	./thumper -n 10000 test.thumper.txt
 	./checker test.thumper.txt
 	rm -rf test.thumper.txt
 
--- thumper.c
+++ thumper.c
@@ -4,7 +4,7 @@
 #include <getopt.h>
 
 const char *cmd = NULL;
-const char shortopts[] = "Fn:";
+const char shortopts[] = "Fn:b:";
 
 #define die(fmt,...) do { \
   fprintf(stderr, "[%s:%i] Error: %s() "fmt"\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
@@ -15,7 +15,8 @@ void print_usage()
 {
   fprintf(stderr, "usage: %s [-F|-n <N>] <out_path>\n", cmd);
   fprintf(stderr, "  Print many lines to a file\n");
-  fprintf(stderr, "  -n <N>    Print N lines [default 10,000]\n");
+  fprintf(stderr, "  -b <N>    Write N blocks of 8k\n");
+  fprintf(stderr, "  -n <N>    Print N lines\n");
   fprintf(stderr, "  -F        Flush after printing each line\n");
   exit(EXIT_FAILURE);
 }
@@ -23,7 +24,7 @@ void print_usage()
 int main(int argc, char **argv)
 {
   bool flush = false;
-  unsigned long i, limit = 10000;
+  unsigned long i, nlines = 0, nblocks = 0;
   cmd = argv[0];
 
   if(argc < 2) print_usage();
@@ -35,28 +36,40 @@ int main(int argc, char **argv)
   while((c = getopt(argc, argv, shortopts)) != -1) {
     switch(c) {
       case 'F': flush = true; break;
-      case 'n': limit = atol(optarg); break;
+      case 'n': nlines = atol(optarg); break;
+      case 'b': nblocks = atol(optarg); break;
       case 'h': print_usage();
       default: print_usage();
     }
   }
 
-  if(optind + 1 != argc) print_usage();
+  if(optind + 1 != argc || (!nlines && !nblocks)) print_usage();
 
   const char *path = argv[optind];
 
   fprintf(stderr, "Running Thumper...\n");
-  fprintf(stdout, "Writing %lu lines to file: %s\n", limit, path);
 
+  const size_t block_len = 1024*2;
   FILE *fout = fopen(path, "w");
   if(!fout) die("Cannot open file: %s", path);
 
-  for(i = 0; i < limit; i++) {
+  if(flush) fprintf(stderr, "Calling flush after every write\n");
+
+  fprintf(stderr, "Writing %lu blocks to file: %s\n", nblocks, path);
+  int *data = calloc(sizeof(int), block_len);
+  for(i = 0; i < block_len; i++) data[i] = i;
+  for(i = 0; i < nblocks; i++) {
+    fwrite(data, sizeof(int), block_len, fout);
+    if(flush) fflush(fout);
+  }
+  free(data);
+
+  fprintf(stderr, "Writing %lu lines to file: %s\n", nlines, path);
+  for(i = 0; i < nlines; i++) {
     fprintf(fout, "%lu\n", i);
     if(flush) fflush(fout);
   }
 
-  if(flush) fflush(fout);
   fclose(fout);
   fprintf(stderr, "Done.\n");
   return EXIT_SUCCESS;
